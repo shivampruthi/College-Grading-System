b81393edbd456397eabb2013ded2d688c57eb18a
Shivam Pruhti <104611097+shivampruthi@users.noreply.github.com>
2023-01-12 14:35:47 +0530

    Update GradingSystem.java

--- GradingSystem.java
+++ GradingSystem.java
@@ -5,95 +5,147 @@
  * @author shivampruthi
  *
  */
-package Project;
-
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.sql.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class GradingSystem extends JFrame implements ActionListener {
+public class CollegeGradingSystem extends JFrame {
+	public static void main(String[] args) {
+		new CollegeGradingSystem();
+	}
 
-    private static final long serialVersionUID = 1L;
-    private JTextField txtMarks1, txtMarks2, txtMarks3, txtMarks4, txtMarks5;
-    private JLabel lblSubject1, lblSubject2, lblSubject3, lblSubject4, lblSubject5, lblGrade;
-    private JButton btnCalculate;
+	public CollegeGradingSystem() {
+		// Create window frame
+		JFrame frame = new JFrame("College Grading System");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 400);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 
-    public GradingSystem() {
-        super("College Grading System");
-        setSize(400, 300);
-        setLocationRelativeTo(null);
-        setResizable(false);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new GridLayout(7, 2));
+		// Create panel
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
 
-        lblSubject1 = new JLabel("Subject 1:");
-        txtMarks1 = new JTextField(10);
-        add(lblSubject1);
-        add(txtMarks1);
+		// Create labels
+		JLabel nameLabel = new JLabel("Name:");
+		nameLabel.setBounds(20, 20, 80, 25);
+		panel.add(nameLabel);
 
-        lblSubject2 = new JLabel("Subject 2:");
-        txtMarks2 = new JTextField(10);
-        add(lblSubject2);
-        add(txtMarks2);
+		JLabel rollLabel = new JLabel("Roll:");
+		rollLabel.setBounds(20, 50, 80, 25);
+		panel.add(rollLabel);
 
-        lblSubject3 = new JLabel("Subject 3:");
-        txtMarks3 = new JTextField(10);
-        add(lblSubject3);
-        add(txtMarks3);
+		JLabel sub1Label = new JLabel("Subject 1:");
+		sub1Label.setBounds(20, 80, 80, 25);
+		panel.add(sub1Label);
 
-        lblSubject4 = new JLabel("Subject 4:");
-        txtMarks4 = new JTextField(10);
-        add(lblSubject4);
-        add(txtMarks4);
+		JLabel sub2Label = new JLabel("Subject 2:");
+		sub2Label.setBounds(20, 110, 80, 25);
+		panel.add(sub2Label);
 
-        lblSubject5 = new JLabel("Subject 5:");
-        txtMarks5 = new JTextField(10);
-        add(lblSubject5);
-        add(txtMarks5);
+		JLabel sub3Label = new JLabel("Subject 3:");
+		sub3Label.setBounds(20, 140, 80, 25);
+		panel.add(sub3Label);
 
-        btnCalculate = new JButton("Calculate");
-        btnCalculate.addActionListener(this);
-        add(btnCalculate);
+		JLabel sub4Label = new JLabel("Subject 4:");
+		sub4Label.setBounds(20, 170, 80, 25);
+		panel.add(sub4Label);
 
-        lblGrade = new JLabel("Grade: ");
-        add(lblGrade);
+		JLabel sub5Label = new JLabel("Subject 5:");
+		sub5Label.setBounds(20, 200, 80, 25);
+		panel.add(sub5Label);
 
-        setVisible(true);
-    }
+		JLabel gradeLabel = new JLabel("Grade:");
+		gradeLabel.setBounds(20, 230, 80, 25);
+		panel.add(gradeLabel);
 
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == btnCalculate) {
-            try {
-                double marks1 = Double.parseDouble(txtMarks1.getText());
-                double marks2 = Double.parseDouble(txtMarks2.getText());
-                double marks3 = Double.parseDouble(txtMarks3.getText());
-                double marks4 = Double.parseDouble(txtMarks4.getText());
-                double marks5 = Double.parseDouble(txtMarks5.getText());
-                double totalMarks = marks1 + marks2 + marks3 + marks4 + marks5;
-                double percentage = (totalMarks / 500) * 100;
+		// Create text fields
+		JTextField nameField = new JTextField(20);
+		nameField.setBounds(100, 20, 165, 25);
+		panel.add(nameField);
 
-                //Calculate Grade
-                String grade;
-                if (percentage >= 80)
-                    grade = "A";
-                else if (percentage >= 70 && percentage < 80)
-                    grade = "B";
-                else if (percentage >= 60 && percentage < 70)
-                    grade = "C";
-                else if (percentage >= 50 && percentage < 60)
-                    grade = "D";
-                else
-                    grade = "F";
+		JTextField rollField = new JTextField(20);
+		rollField.setBounds(100, 50, 165, 25);
+		panel.add(rollField);
 
-                lblGrade.setText("Grade: " + grade);
-            } catch (NumberFormatException ex) {
-                JOptionPane.showMessageDialog(this, "Please enter valid marks", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-    }
-    public static void main(String[] args) {
-        GradingSystem gradingSystem = new GradingSystem();
-    }
+		JTextField sub1Field = new JTextField(20);
+		sub1Field.setBounds(100, 80, 165, 25);
+		panel.add(sub1Field);
 
+		JTextField sub2Field = new JTextField(20);
+		sub2Field.setBounds(100, 110, 165, 25);
+		panel.add(sub2Field);
+
+		JTextField sub3Field = new JTextField(20);
+		sub3Field.setBounds(100, 140, 165, 25);
+		panel.add(sub3Field);
+
+		JTextField sub4Field = new JTextField(20);
+		sub4Field.setBounds(100, 170, 165, 25);
+		panel.add(sub4Field);
+
+		JTextField sub5Field = new JTextField(20);
+		sub5Field.setBounds(100, 200, 165, 25);
+		panel.add(sub5Field);
+
+		JTextField gradeField = new JTextField(20);
+		gradeField.setBounds(100, 230, 165, 25);
+		panel.add(gradeField);
+
+		// Create button
+		JButton submitButton = new JButton("Submit");
+		submitButton.setBounds(100, 270, 80, 25);
+		panel.add(submitButton);
+
+		// Create action listner
+		submitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = nameField.getText().trim();
+				String roll = rollField.getText().trim();
+				int sub1 = Integer.parseInt(sub1Field.getText().trim());
+				int sub2 = Integer.parseInt(sub2Field.getText().trim());
+				int sub3 = Integer.parseInt(sub3Field.getText().trim());
+				int sub4 = Integer.parseInt(sub4Field.getText().trim());
+				int sub5 = Integer.parseInt(sub5Field.getText().trim());
+
+				// Calculate the grade
+				int avg = (sub1 + sub2 + sub3 + sub4 + sub5)/5;
+				String grade;
+				if (avg >= 90) {
+					grade = "A+";
+				} else if (avg >= 80) {
+					grade = "A";
+				} else if (avg >= 70) {
+					grade = "B+";
+				} else if (avg >= 60) {
+					grade = "B";
+				} else if (avg >= 50) {
+					grade = "C";
+				} else if (avg >= 40) {
+					grade = "D";
+				} else {
+					grade = "F";
+				}
+
+				// Store the data in the database
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/college", "root", "");
+					Statement statement = connection.createStatement();
+					String query = "INSERT INTO student_grades (name, roll, sub1, sub2, sub3, sub4, sub5, grade) VALUES ('" + name + "', '" + roll + "', '" + sub1 + "', '" + sub2 + "', '" + sub3 + "', '" + sub4 + "', '" + sub5 + "', '" + grade + "')";
+					statement.executeUpdate(query);
+					statement.close();
+					connection.close();
+					gradeField.setText(grade);
+					JOptionPane.showMessageDialog(null, "Data inserted successfully!");
+				} catch (ClassNotFoundException ex) {
+					ex.printStackTrace();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+	}
 }
